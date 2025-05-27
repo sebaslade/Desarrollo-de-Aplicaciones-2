8e2d2850c120d9e66f01c77c6c266a24d0e5d0cd
sebaslade <sebaslade29@gmail.com>
2025-05-27 13:09:08 -0500

    Sesion 7

---
+++ Sesion07/Solution_Test_Publicacion/Cliente_WEB_Ventas/WebListarVendedor.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Cliente_WEB_Ventas
+{
+    public partial class WebListarVendedor : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ProxyVendedor.ServicioVendedorClient objServicioVendedor = new ProxyVendedor.ServicioVendedorClient();
+            GridView1.DataSource = objServicioVendedor.ListarVendedor();
+            GridView1.DataBind();
+        }
+    }
+}
