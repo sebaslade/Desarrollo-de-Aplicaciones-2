880b7e2a80f2367beca003a6a2fe55f3a47aa5bc
sebaslade <sebaslade29@gmail.com>
2025-05-27 13:06:59 -0500

    Sesion 2

---
+++ Sesion02/ClienteWindowsNetFramework/ClienteWindowsNetFramework/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ClienteWindowsNetFramework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPlaca = new System.Windows.Forms.TextBox();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblMulta = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(175, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese placa:";
+            // 
+            // txtPlaca
+            // 
+            this.txtPlaca.Location = new System.Drawing.Point(281, 66);
+            this.txtPlaca.Name = "txtPlaca";
+            this.txtPlaca.Size = new System.Drawing.Size(161, 20);
+            this.txtPlaca.TabIndex = 1;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(500, 64);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultar.TabIndex = 2;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(175, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Multa:";
+            // 
+            // lblMulta
+            // 
+            this.lblMulta.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMulta.Location = new System.Drawing.Point(281, 116);
+            this.lblMulta.Name = "lblMulta";
+            this.lblMulta.Size = new System.Drawing.Size(164, 22);
+            this.lblMulta.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 196);
+            this.Controls.Add(this.lblMulta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.txtPlaca);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculo de Multa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPlaca;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblMulta;
+    }
+}
+
---
+++ Sesion02/ClienteWindowsNetFramework/ClienteWindowsNetFramework/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Agregar 
+using ClienteWindowsNetFramework.ServiceAuto;
+
+namespace ClienteWindowsNetFramework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //validacion
+                if(txtPlaca.Text.Trim() == String.Empty)
+                {
+                    throw new Exception("Debe ingresar placa vehicular");
+                }
+
+                // Creamos instancia de servicio WS_Auto.asmx
+                WS_AutoSoapClient objWSAuto = new WS_AutoSoapClient();
+
+                //obtenemos la multa..
+                Single sngMulta = objWSAuto.CalcularMulta(txtPlaca.Text.Trim());
+                lblMulta.Text = sngMulta.ToString("###,###,##0.00");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
